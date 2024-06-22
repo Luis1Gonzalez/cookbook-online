eff6ad7f61f8b8a924c7686efba704f8d1e12f96
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-06-22 10:00:32 +0200

    mejorando el bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Core from "./Core";
 
 function App() {
   return (
-    <>
+    <div className="bg-gradient-to-l from-slate-300 to-sky-100 h-screen">
       <Core />
-    </>
+    </div>
   );
 }
 
--- src/Core.tsx
+++ src/Core.tsx
@@ -5,7 +5,7 @@ import Results from "./components/Results"
 
 const Core = () => {
   return (
-    <div className="min-w-[320px] bg-gradient-to-l from-slate-300 to-sky-100 h-full">
+    <div className="min-w-[320px] bg-gradient-to-l from-slate-300 to-sky-100 h-auto">
         <Header />
         <Form />
         <Results />
