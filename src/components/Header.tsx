a998c1d58b9c00a06eedc8677a22ff043bfe8f79
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-06-22 10:36:20 +0200

    mejorando el bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div className='w-full text-center p-8 text-white text-4xl lg:text-6xl font-semibold bg-gradient-to-r from-cyan-500 to-indigo-500'>
-        <h1>Recetario O<span className='text-3xl italic text-gray-300'>nline</span></h1>
+        <h1>CookBook O<span className='text-3xl italic text-gray-300'>nline</span></h1>
     </div>
   )
 }
